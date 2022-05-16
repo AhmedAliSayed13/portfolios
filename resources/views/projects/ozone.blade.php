d195518ec3b8da102c8b9b4249bfa01bc0433ed2
ahmed ali <68017997+pksaudi4@users.noreply.github.com>
2022-05-16 14:15:49 +0200

    ozone updated

--- resources/views/projects/ozone.blade.php
+++ resources/views/projects/ozone.blade.php
@@ -39,7 +39,7 @@
 
 
 
-                <?php for ($i=1;$i<=17;$i++){?>
+                <?php for ($i=1;$i<=34;$i++){?>
                 <div class="col-lg-4 mt-4 float-left col-md-12 col-xs-12 item-filter playful scrollbar style-2 ozone">
                     <figure class="softeffect">
                         <img src="{{ asset('imgs/portfolio/ozone/' . $i . '.png') }}"
@@ -75,7 +75,7 @@
     <!-- Modal -->
 
 
-    <?php for ($i=1;$i<=17;$i++){?>
+    <?php for ($i=1;$i<=34;$i++){?>
     <div class="modal fade" id="Modal_ozone_<?php echo $i; ?>" tabindex="-1" role="dialog"
         aria-labelledby="Modal_ozone_<?php echo $i; ?>" aria-hidden="true">
         <div class="modal-dialog" role="document">
